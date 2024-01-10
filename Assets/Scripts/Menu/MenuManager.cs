7a45e0845d1ff067de61b87cbbadf0be453fe50c
barkinspmz <barkinsapmaz2@outlook.com>
2024-01-10 13:25:03 +0300

    StartMenu

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] private GameObject creditCanvas;
+    [SerializeField] private GameObject optionsCanvas;
+    [SerializeField] private GameObject mainMenuCanvas;
     public void ClickPlay()
     {
         var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
@@ -12,7 +15,21 @@ public class MenuManager : MonoBehaviour
 
     public void ClickCredits()
     {
+        mainMenuCanvas.SetActive(false);
+        creditCanvas.SetActive(true);   
+    }
 
+    public void ClickOptions()
+    {
+        optionsCanvas.SetActive(true);
+        mainMenuCanvas.SetActive(false);
+    }
+
+    public void ClickBackToMainMenu()
+    {
+        optionsCanvas.SetActive(false);
+        creditCanvas.SetActive(false);
+        mainMenuCanvas.SetActive(true);
     }
 }
             
